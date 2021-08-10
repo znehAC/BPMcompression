1a0441e97924ea0b8ba2c4501d82ae33d1b4683d
Adriano Cesar <adrianocesar321@gmail.com>
2021-08-09 21:15:01 -0300

    finished DCT+RLE

--- dct.py
+++ dct.py
@@ -1,6 +1,18 @@
+from numpy.core.defchararray import index
+from main import encode_header
 import numpy as np
 import math
 import cv2
+from numpy.core.fromnumeric import shape
+
+QUANT_MATRIX = [[16, 11, 10, 16, 24, 40, 51, 61],
+              [12, 12, 14, 19, 26, 58, 60, 55],
+              [14, 13, 16, 24, 40, 57, 69, 56],
+              [14, 17, 22, 29, 51, 87, 80, 62],
+              [18, 22, 37, 56, 68, 109, 103, 77],
+              [24, 35, 55, 64, 81, 104, 113, 92],
+              [49, 64, 78, 87, 103, 121, 120, 101],
+              [72, 92, 95, 98, 112, 100, 103, 99]]
 
 def to_binary_list(n):
     """Convert integer into a list of bits"""
@@ -106,66 +118,211 @@ def inverseDCT(qm, m, mt, block):
 
     return np.matmul(np.matmul(mt, block), m)
 
+def zigzag(m):
+    count = 1
+    direction = -1
+    result = []
+    currentX = 0
+    currentY = 0
+    n = shape(m)[0]*2 - 1
+    for i in range(n):
+        for x in range(count):
+            result.append(int(m[currentY, currentX]))
+            if x == count -1:
+                break
+            elif direction == 1:
+                currentY -= 1
+                currentX += 1
+            elif direction == -1:
+                currentY += 1
+                currentX -= 1
+        if i < math.floor(n/2) :
+            if direction == 1:
+                currentX += 1
+                direction = -1
+            elif direction == -1:    
+                currentY += 1
+                direction = 1
+            count += 1
+        else:
+            if direction == 1:
+                currentY += 1
+                direction = -1
+            elif direction == -1:
+                currentX += 1
+                direction = 1
+            count -= 1  
+    return result
 
+def reverseZigzag(zz):
+    count = 1
+    arrayPosition = 0
+    direction = -1
+    size = int(math.sqrt(len(zz)))
+    result = np.zeros((size, size))
+    currentX = 0
+    currentY = 0
+    n = size*2 - 1
+    for i in range(n):
+        for x in range(count):
+            result[currentY, currentX] = float(zz[arrayPosition])
+            arrayPosition += 1
+            if x == count -1:
+                break
+            elif direction == 1:
+                currentY -= 1
+                currentX += 1
+            elif direction == -1:
+                currentY += 1
+                currentX -= 1
+        if i < math.floor(n/2) :
+            if direction == 1:
+                currentX += 1
+                direction = -1
+            elif direction == -1:    
+                currentY += 1
+                direction = 1
+            count += 1
+        else:
+            if direction == 1:
+                currentY += 1
+                direction = -1
+            elif direction == -1:
+                currentX += 1
+                direction = 1
+            count -= 1 
+    return result
 
-def main():
-    filename = 'lena512.bmp'
-    out_filename = 'teste.bmp'
+def rLEncode(array):
+    lastNonZero = 0
+    zerosCount = 0
+    result = []
+    for i in range(len(array)-1, -1, -1):
+        if array[i] != 0:
+            lastNonZero = i
+            break
+        zerosCount += 1
+    result.append(lastNonZero+1)
+    for i in range(lastNonZero+1):
+        result.append(array[i])
+    result.append(zerosCount)
+    return result
+
+def reverseRLE(array):
+    result = []
+    zerosCount = array[len(array)-1]
+    for i in range(array[0]):
+        result.append(array[i+1])
+    for i in range(zerosCount):
+        result.append(0)
+    return result
+
+def convertToByteArray(a):
+    result = []
+    for i in range(len(a)):
+        # result[i] = [to_binary_list(a[i])]
+        if a[i] < 0:
+            a[i] = a[i]*-1
+        result.append(pad_bits(to_binary_list(a[i]), 8))
+    return result
+
+def encode_block(block):
+    zz = zigzag(block)
+    rle = rLEncode(zz)
+    return rle
+
+def encode_pixels(img, hw, stream):
+    m = generateTransformMatrix()
+    mt = np.transpose(m)
+    height, width = hw
+    for i in range(0, height, 8):
+        for j in range(0, width, 8):
+            block = img[i:i+8, j:j+8]                       #a 8x8 block
+            dct = applyDCT(m, mt, block)                    #applying the dct into the matrix
+            block = np.around(np.divide(dct, QUANT_MATRIX)) #applying quantization 
+            eBlock = encode_block(block)                    #scanning the matrix in zigzag and RL encoding it
+            bytes = convertToByteArray(eBlock)              #converting to an array of array of bits
+            for byte in bytes:
+                stream.write_bits(byte)
+
+def encode_header(header, stream):
+    stream.write_bits(header)
+
+def compressImage(filename, out_filename):
+    img = cv2.imread(filename, 0)
+    
+    height, width = img.shape[:2]
 
     bytes_array = np.fromfile(filename, dtype = "uint8")
     bits = np.unpackbits(bytes_array)
+
     header = bits[:54*8]
     header = list(header)
 
-    img = cv2.imread(filename, 0)
+    stream = OutputBitStream(out_filename)
 
-    height, width = img.shape[:2]
+    encode_header(header, stream)
+    stream.flush()
+    encode_pixels(img, [height, width], stream)
+    stream.close()
 
-    quant_matrix = [[16, 11, 10, 16, 24, 40, 51, 61],
-              [12, 12, 14, 19, 26, 58, 60, 55],
-              [14, 13, 16, 24, 40, 57, 69, 56],
-              [14, 17, 22, 29, 51, 87, 80, 62],
-              [18, 22, 37, 56, 68, 109, 103, 77],
-              [24, 35, 55, 64, 81, 104, 113, 92],
-              [49, 64, 78, 87, 103, 121, 120, 101],
-              [72, 92, 95, 98, 112, 100, 103, 99]]
+def decode_block(stream):
+    sequence = []
 
+    numberCount = from_binary_list(stream.read_bits(8))
+    sequence.append(numberCount)
+
+    for i in range(numberCount):
+        number = from_binary_list(stream.read_bits(8))
+        sequence.append(number)
+    zeroCount = from_binary_list(stream.read_bits(8))
+    sequence.append(zeroCount)
+    block = reverseRLE(sequence)
+    block = reverseZigzag(block)
+    return block
+def decode_pixels(hw, stream):
     m = generateTransformMatrix()
-    
     mt = np.transpose(m)
-    
-    temp = np.zeros((width, height))
+    height, width = hw
     nimg = np.zeros((width, height))
-
-    #FAZ O DCT
     for i in range(0, height, 8):
         for j in range(0, width, 8):
-
-            block = img[i:i+8, j:j+8]
-            dct = applyDCT(m, mt, block)
-            block = np.around(np.divide(dct, quant_matrix))
-
-
-            temp[i:i+8, j:j+8] = block
-
-    #REVERTE O DCT
-    for i in range(0, height, 8):
-        for j in range(0, width, 8):
-            block = temp[i:i+8, j:j+8]
-
-
-            idct = inverseDCT(quant_matrix, m, mt, block)
+            block = decode_block(stream)
+            idct = inverseDCT(QUANT_MATRIX, m, mt, block)
 
             block = np.uint8(np.around(idct))
-
             nimg[i:i+8, j:j+8] = block
+    return nimg
 
-    print("\nImagem Original")
-    print(img)
-    print("\nImagem apos o DCT")
-    print(temp)
-    print("\nImagem ao reverter o DCT")
-    print(nimg)
+def decompressImage(filename, out_filename):
+    bytes_array = np.fromfile(filename, dtype = "uint8")
+    bits = np.unpackbits(bytes_array)
+
+    #WIDTH  =  (17*8):(21*8)
+    #HEIGHT = (21*8):(25*8)
+    header = bits[:54*8]
+    w = header[(17*8):(21*8)]
+    h = header[(21*8):(25*8)]
+    width = from_binary_list(w)
+    height = from_binary_list(h)
+    
+    stream = InputBitStream(filename)
+
+    header = stream.read_bits(54*8)
+    stream.flush()
+
+    img = decode_pixels([height, width], stream)
+    stream.flush()
+
+    cv2.imwrite(out_filename, img)
+    
+def main():
+    filename = 'lena512.bmp'
+    c_filename = 'lena512.dct'
+    out_filename = 'teste.bmp'
+
+    compressImage(filename, c_filename)
+    decompressImage(c_filename, out_filename)
 
 
 
